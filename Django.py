5030881e4ce57d7e9b950502576f9ff777e857a9
RebeckaRebecka <rebeckasax@me.com>
2018-03-24 10:48:30 +0100

    My Django Girls app, first commit

---
+++ Django.py
@@ -0,0 +1,7 @@
+print('Hello, Django girls!')
+name = 'Rebecka'
+if 3 > 2: 
+	print('It works!')
+
+print(name)
+
---
+++ exercices/Django.py
@@ -0,0 +1,24 @@
+def hi(name): 
+	print('Hi ' + name + '!')
+
+hi("Rebecka")	
+
+print('Hello, Django girls!')
+name = 'Rebecka'
+if 3 > 2: 
+	print('It works!')
+
+print(name)
+name2 = 'Isabell'
+print(name2)
+
+girls = ['Agathe', 'Isabell', 'Rebecka']
+
+for name in girls: 
+	hi(name)
+	print(' ')
+
+
+
+
+
